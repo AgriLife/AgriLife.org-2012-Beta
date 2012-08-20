d42bf8e982bf65d272498c4a5c9e8a1d1b8742ad
J. Aaron Eaton <aaron@channeleaton.com>
2012-08-20 15:41:32 -0500

    Created initial page templates for the 5 branches

---
+++ page-templates/extension.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template name: Extension Template
+ */
+
+get_header(); ?>
+
+<div class="content-wrap">
+	<section id="content" role="main" class="two-of-3 column">
+
+				<?php the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php comments_template( '', true ); ?>
+
+
+	</section><!-- /end #content -->
+
+<?php dynamic_sidebar( 'sidebar_extension' ); ?>
+	
+</div><!-- /.content-wrap -->
+
+<?php get_footer(); ?>
