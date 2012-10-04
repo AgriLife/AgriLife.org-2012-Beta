9793a43d6c6e6043076ebef73a34948878c24c70
J. Aaron Eaton <aaron@channeleaton.com>
2012-10-04 15:16:28 -0500

    Google Search Appliance is working inside iframe

--- searchform.php
+++ searchform.php
@@ -7,8 +7,10 @@
  * @since agrilifeorg 1.0
  */
 ?>
-	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<form method="get" id="searchform" target="GoogleSearch" action="http://search.tamu.edu/search">
 		<label for="s" class="assistive-text"><?php _e( 'Search', 'agrilifeorg' ); ?></label>
-		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search', 'agrilifeorg' ); ?>" />
+		<input type="text" class="field" name="q" id="s" placeholder="<?php esc_attr_e( 'Search', 'agrilifeorg' ); ?>" />
+    <input type="hidden" name="site" value="ag_collection" />
+    <input type="hidden" name="proxystylesheet" value="ag_frontend" />
 		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'agrilifeorg' ); ?>" />
 	</form>
