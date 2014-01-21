7b079572e25ced28d36646a160dc84d5dc44df22
Travis Ward <travis@travsiward.com>
2014-01-21 13:10:04 -0600

    interim dean fix

--- page-templates/college-landing.php
+++ page-templates/college-landing.php
@@ -24,9 +24,11 @@ get_header(); ?>
 	<section id="sidebar" class="sidebar one-of-3 column" role="navigation">
 		<aside id="director_bio" class="widget interior-sidebar">
 			<div class="widget-wrap director-bio-wrap">
-				<img src="<?php bloginfo('template_directory'); ?>/images/landing/hussey.jpg" alt="Dr. Hussey" class="director-mug" title="Dr. Hussey" />
+				<!--<img src="<?php bloginfo('template_directory'); ?>/images/landing/hussey.jpg" alt="Dr. Hussey" class="director-mug" title="Dr. Hussey" />
 				<h2>Dr. Mark A. Hussey,<br />
-					Vice Chancellor and Dean</h2>
+					Vice Chancellor and Dean</h2>-->
+				<img src="//agrilifecdn.tamu.edu/wp-content/uploads/BillDugas1.jpg" alt="Dr. Doug Steele" class="director-mug" title="Dr. Doug Steele" />
+				<h2>Dr. Bill Dugas,<br />Acting Vice Chancellor and Dean</h2>
 				<h4 class="landing-contact">Contact</h4>
 				<p>Agriculture and Life Sciences Building<br />
 				600 John Kimbrough Blvd., Suite 510<br />
@@ -35,7 +37,7 @@ get_header(); ?>
 				<h4 class="landing-contact">Phone</h4>
 				<a class="phone" href="tel://979.845.3712">979.845.3712</a>
 				
-				<a href="http://agrilife.org/about/leadership/markhussey/"><img src="<?php bloginfo('template_directory'); ?>/images/landing/learn-hussey.png" alt="Dr. Hussey Bio" class="director-bio-btn" title="Dr. Hussey" /></a>
+				<!-- <a href="http://agrilife.org/about/leadership/markhussey/"><img src="<?php bloginfo('template_directory'); ?>/images/landing/learn-hussey.png" alt="Dr. Hussey Bio" class="director-bio-btn" title="Dr. Hussey" /></a>-->
 				<a href="http://aglifesciences.tamu.edu/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/landing/explore-college.png" alt="Texas A&amp;M AgriLife Research" class="agency-link-btn" title="Texas A&amp;M AgriLife Research" /></a>
 				
 			</div>
